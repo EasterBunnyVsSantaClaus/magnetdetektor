983e3d7551106658e24e95bee15769b1a03a64c2
Michael Fuhrmann <michaelfuhrmann@Freigabe.local>
2014-09-28 02:11:51 +0200

    finish upload
    Grundprinzip nachgebildet.

--- magnetdetektor/ViewController.swift
+++ magnetdetektor/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    //Labels & ProgressBar initialisieren bzw. definieren
+    @IBOutlet weak var labelStatusText: UILabel!
+    @IBOutlet weak var labelStatusZahl: UILabel!
+    @IBOutlet var progressStatusBar: UIProgressView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
+        let magnitude:Double = sqrt(pow(newHeading.x, 2) + pow(newHeading.y, 2) + pow(newHeading.z, 2))
+        let magnitudeRatio = Float(magnitude) / 600
+        progressStatusBar.progress = magnitudeRatio
+        
+        //Link: http://www.cocoa-coding.de/zeichenketten/nsstring.html
+        var formatter:NSString = NSString(format: "%.3f", magnitudeRatio)
+        
+        labelStatusZahl.text = formatter
+        
+        //Labeltext setzen (Kann ev. ausgelagert werden!?)
+        if magnitudeRatio > 2 {
+            labelStatusText.text = "Grossen Magnet gefunden :D"
+        } else if magnitudeRatio > 0.9 {
+            labelStatusText.text = "Magnet gefunden :)"
+        } else {
+            labelStatusText.text = "Keinen Magnet gefunden :("
+        }
     }
-
-
+    
+    @IBAction func buttonReset(sender: AnyObject) {
+        labelStatusZahl.text = "0.000"
+    }
 }
 
